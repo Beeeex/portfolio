f0d307b8a8aeccd614baac85484500c627220583
Beeeex <massimiliano.bassan25@gmail.com>
2021-11-04 17:31:32 +0100

    ImgCards update

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -2,20 +2,25 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { BiLinkExternal } from "react-icons/bi";
+import { Col, Row } from "react-bootstrap";
 
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img
-        variant="top"
-        src={props.imgPath}
-        alt="card-img"
-        style={{
-          maxHeight: 240,
-          maxWidth: 250,
-          marginLeft: 45,
-        }}
-      />
+      <Row className="justify-content-md-center">
+        <Col>
+          <Card.Img
+            variant="top"
+            src={props.imgPath}
+            alt="card-img"
+            style={{
+              maxHeight: 240,
+              maxWidth: 250,
+            }}
+          />
+        </Col>
+      </Row>
+
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
